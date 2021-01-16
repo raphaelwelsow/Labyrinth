e3185bc239918d4668732b952258cfbe4431759d
straussss <75756701+straussss@users.noreply.github.com>
2021-01-16 19:03:36 +0100

    Nicht mehr relevante Test-Befehle entfernt

--- UmbenennenMain.java
+++ UmbenennenMain.java
@@ -6,24 +6,27 @@ import java.awt.event.KeyEvent;
 
 public class UmbenennenMain {
 
-	 private static BoardController controller;
-	 private static KeyBuffer buffer;
-	 private static int xPos, yPos;
-	 public static int[]posi = new int[2];
+	private static BoardController controller;
+	private static KeyBuffer buffer;
+	private static int xPos, yPos;
+	public static int[] posi = new int[2];
 
 	public static void main(String[] args) {
-		//größeres Board falls gewünscht
+		// größeres Board falls gewünscht
 		BoardController.getBoardController(LedConfiguration.LED_20x20_EMULATOR);
 		BoardController controller = BoardController.getBoardController();
-		
+
 		Sammlung.SammlungErstellen();
 		controller = BoardController.getBoardController();
 		buffer = controller.getKeyBuffer();
-		
-		//Startposition des Punktes festlegen
-		
-		LabyrinthElement aktLabyrinth = Sammlung.main.getLabyrinth(1);
-		posi = aktLabyrinth.getStart();//übergibt den Startwert (x) des Labyrinths
+
+		Text.showTeam();
+		Text.erklärung();
+
+		// Startposition des Punktes festlegen
+		LabyrinthElement aktLabyrinth = Sammlung.main.getLabyrinth(0);
+		System.out.print(aktLabyrinth.getEnd()[0] + aktLabyrinth.getEnd()[1]);
+		posi = aktLabyrinth.getStart();// übergibt den Startwert (x) des Labyrinths
 		xPos = posi[0];
 		yPos = posi[1];
 		System.out.println(xPos);
@@ -32,61 +35,61 @@ public class UmbenennenMain {
 
 		while (true) {
 			KeyEvent event = buffer.pop();
-			//der Buffer speichert das letzte Key-Event (also die letzte Tastatureingabe zwischen) und wird dann ausgelesen
-			
-			if(event != null)
-			{
+			// der Buffer speichert das letzte Key-Event (also die letzte Tastatureingabe
+			// zwischen) und wird dann ausgelesen
+
+			if (event != null) {
 				if (event.getID() == java.awt.event.KeyEvent.KEY_PRESSED)
-					//hier wird überprüft, ob eine Taste gedrückt wurde
+				// hier wird überprüft, ob eine Taste gedrückt wurde
 				{
 					switch (event.getKeyCode()) {
 					case java.awt.event.KeyEvent.VK_UP:
 						int yPosU = yPos;
-						if(yPos >= 0 && aktLabyrinth.zulaessig(xPos,-- yPosU) == true)
-						{
+						if (yPos >= 0 && aktLabyrinth.zulaessig(xPos, --yPosU) == true) {
 							yPos--;
-							System.out.println(yPos);
 						}
 						break;
-						
+
 					case java.awt.event.KeyEvent.VK_DOWN:
 						int yPosD = yPos;
-						if (yPos <= 19 && aktLabyrinth.zulaessig(xPos,++ yPosD)==true)
-						{
+						if (yPos <= 19 && aktLabyrinth.zulaessig(xPos, ++yPosD) == true) {
 							yPos++;
-							System.out.println(yPos);
 						}
 						break;
-						
+
 					case java.awt.event.KeyEvent.VK_LEFT:
 						int xPosL = xPos;
-						if(xPos >= 0 && aktLabyrinth.zulaessig(-- xPosL, yPos)== true)
-						{
+						if (xPos >= 0 && aktLabyrinth.zulaessig(--xPosL, yPos) == true) {
 							xPos--;
-							System.out.println(xPos);
 						}
 						break;
-						
+
 					case java.awt.event.KeyEvent.VK_RIGHT:
 						int xPosR = xPos;
-						if(xPos <= 19 && aktLabyrinth.zulaessig(++ xPosR, yPos)== true)
-						{
+						if (xPos <= 19 && aktLabyrinth.zulaessig(++xPosR, yPos) == true) {
 							xPos++;
-							System.out.println(xPos);
 						}
 						break;
 					default:
-						
+
 					}
 				}
-			}
-			//controller.resetColors();
-			//controller.setColor(xPos, yPos, 127,0,0);
-			//controller.updateBoard();
-			ShowLabyrinth.updatePosition(new int[]{xPos,yPos}, aktLabyrinth);
-			
-		}
-		
-	}
 
+			}
+
+			ShowLabyrinth.updatePosition(new int[] { xPos, yPos }, aktLabyrinth);
+			if (aktLabyrinth.getNext() != null && xPos == aktLabyrinth.getEnd()[0]  && yPos == aktLabyrinth.getEnd()[1]){
+				Text.showEnd(aktLabyrinth.getNext().getLevel());
+				aktLabyrinth = aktLabyrinth.getNext();
+				System.out.print("Ziel erreicht!");
+				posi = aktLabyrinth.getStart();// übergibt den Startwert (x) des Labyrinths
+				xPos = posi[0];
+				yPos = posi[1];
+				//System.out.println(xPos);
+				//System.out.println(yPos);
+				ShowLabyrinth.showLabyrinth(aktLabyrinth);
+			}
+		}
+
+	}
 }
